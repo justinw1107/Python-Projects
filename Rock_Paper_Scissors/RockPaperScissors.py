f239dfdeba1ea94bb7d9be0ec10c6968e632bc37
justinw1107 <justinwan1107@gmail.com>
2023-10-12 23:23:13 +0100

    added global variables and compute round method

--- Rock_Paper_Scissors/RockPaperScissors.py
+++ Rock_Paper_Scissors/RockPaperScissors.py
@@ -1,17 +1,58 @@
 import random
 
 class My_class:
-  def pick_hand(self, player, computer):
+  def __init__(self):
+    self.player = ''
+    self.computer = ''
+    self.result = ''
+    self.total = []
+  
+  def pick_hands(self):
     rps_outcomes = ('rock', 'paper', 'scissors')
-    x, y = random.randint(1,3)
-    player = rps_outcomes[x]
-    computer = rps_outcomes[y]
+    x = random.randint(0,2)
+    y = random.randint(0,2)
+    self.player = rps_outcomes[x]
+    self.computer = rps_outcomes[y]
+    print(self.player, self.computer)
     
-    return player, computer
+    
+  def compute_round(self, rounds):
+    for x in range(rounds):
+      self.pick_hands()
+      
+      if self.player == 'rock':
+        if self.computer == 'rock':
+          self.result = 'tie'
+        elif self.computer == 'paper':
+          self.result = 'lose'
+        else:
+          self.result = 'win'
+      elif self.player == 'paper':
+        if self.computer == 'rock':
+          self.result = 'win'
+        elif self.computer == 'paper':
+          self.result = 'tie'
+        else:
+          self.result = 'lose'
+      else:
+        if self.computer == 'rock':
+          self.result = 'lose'
+        elif self.computer == 'paper':
+          self.result = 'win'
+        else:
+          self.result = 'tie'
+          
+      self.total.append(self.result)
     
     
-  # def compute_round(self, rounds*):
+  def add_result(self):
+    self.compute_round(10)
+    for x in range(self.total):
+      print(self.total[x])
+      
+    
+            
     
 
 obj = My_class()
-obj.instance_method()
+obj.add_result()
